e94e002a4cb213a0bc9777af379407bffa098f49
oh-chaeyeon <chaebread555@gmail.com>
2025-03-07 00:46:13 +0900

    feat : 회원가입 시 사용되는 드롭다운 컴포넌트 구현

---
+++ src/pages/signup/Dropdown.js
@@ -0,0 +1,54 @@
+import { useState, useEffect, useRef } from "react";
+import { ChevronDown, ChevronUp } from "lucide-react"; 
+
+export default function Dropdown({ label, options, selected, setSelected, placeholder }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  // Dropdown 바깥을 클릭하면 닫히도록 설정
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleOptionClick = (option) => {
+    setSelected(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="relative w-full" ref={dropdownRef}>
+      <label className="block mb-1 text-sm font-medium text-gray-700">{label}</label>
+      <div
+        className={`flex items-center justify-between w-full h-12 px-4 py-2 border border-2.5 cursor-pointer rounded-xl text-base ${selected ? 'text-black-700' : 'text-[#9FA6B2]'} bg-black-50`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span>{selected || placeholder}</span>
+        <span>
+          {isOpen ? <ChevronUp className="w-4 h-4" /> : <ChevronDown className="w-4 h-4" />}
+        </span>
+      </div>
+      {isOpen && (
+        <ul className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-lg shadow-md bg-black-50">
+          {options.map((option) => (
+            <li
+              key={option}
+              className="px-4 py-2 border rounded-lg cursor-pointer border-black-50 hover:bg-gray-100"
+              onClick={() => handleOptionClick(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
