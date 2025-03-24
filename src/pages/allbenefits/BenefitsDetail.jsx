6951ec4b3eb5ca8586547f117a4139adab30871a
Seungeun <sdf3872@gmail.com>
2025-03-24 14:11:25 +0900

    feat: 경고 메세지 수정

--- src/pages/allbenefits/BenefitsDetail.jsx
+++ src/pages/allbenefits/BenefitsDetail.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { benefitDetailData } from "../../data/benefitDetailData";
-import { BookmarkIcon } from "@heroicons/react/24/solid";
 
 const BenefitsDetail = () => {
   const { id } = useParams();
@@ -25,22 +24,22 @@ const BenefitsDetail = () => {
       <div className="flex flex-col gap-6">
         <div className="flex min-w-[322px] min-h-[40px]">
           {/* 왼쪽 '담당부서' 박스 */}
-          <div className="bg-yellow-700 text-black-50 text-lg px-4 py-2 flex items-center">
+          <div className="flex items-center px-4 py-2 text-lg bg-yellow-700 text-black-50">
             담당부서
           </div>
 
           {/* 가운데 부서명 */}
-          <div className="bg-white text-black text-lg px-4 py-2 flex items-center border-2 border-yellow-700">
+          <div className="flex items-center px-4 py-2 text-lg text-black bg-white border-2 border-yellow-700">
             {department}
           </div>
 
           {/* 오른쪽 연락처 */}
-          <div className="bg-white text-black text-lg px-4 py-2 flex items-center border-2 border-yellow-700">
+          <div className="flex items-center px-4 py-2 text-lg text-black bg-white border-2 border-yellow-700">
             {phone}
           </div>
         </div>
         {/* 헤더 섹션 */}
-        <div className="mb-6 flex flex-col">
+        <div className="flex flex-col mb-6">
           <span className="text-[36px] font-bold">{serviceName}</span>
           <span className="text-[24px]">{servicePurpose}</span>
         </div>
@@ -56,8 +55,8 @@ const BenefitsDetail = () => {
           </div>
           <div className="mt-[18px] space-y-4 ml-8">
             <div className="flex flex-col">
-              <li className="font-medium mb-2">지원대상 : {details.supportTarget}</li>
-              <li className="font-medium mb-2">선정기준 : {details.selectionCriteria}</li>
+              <li className="mb-2 font-medium">지원대상 : {details.supportTarget}</li>
+              <li className="mb-2 font-medium">선정기준 : {details.selectionCriteria}</li>
             </div>
           </div>
         </div>
@@ -70,8 +69,8 @@ const BenefitsDetail = () => {
           </div>
           <div className="mt-[18px] space-y-4 ml-8">
             <div className="flex flex-col">
-              <li className="font-medium mb-2">지원내용 : {details.supportDetails}</li>
-              <li className="font-medium mb-2">지원유형 : {details.supportType}</li>
+              <li className="mb-2 font-medium">지원내용 : {details.supportDetails}</li>
+              <li className="mb-2 font-medium">지원유형 : {details.supportType}</li>
             </div>
           </div>
         </div>
@@ -84,16 +83,16 @@ const BenefitsDetail = () => {
           </div>
           <div className="mt-[18px] space-y-4 ml-8">
             <div className="flex flex-col">
-              <li className="font-medium mb-2">신청방법 : {details.applicationMethod}</li>
-              <li className="font-medium mb-2">신청기한 : {details.applicationDeadline}</li>
-              <li className="font-medium mb-2">구비서류 : {details.requiredDocuments.join(', ')}</li>
+              <li className="mb-2 font-medium">신청방법 : {details.applicationMethod}</li>
+              <li className="mb-2 font-medium">신청기한 : {details.applicationDeadline}</li>
+              <li className="mb-2 font-medium">구비서류 : {details.requiredDocuments.join(', ')}</li>
             </div>
           </div>
         </div>
       </div>
 
       {/* 신청하기 버튼 */}
-      <div className="mt-8 justify-end flex">
+      <div className="flex justify-end mt-8">
         <button 
           className="px-12 max-w-[237px] bg-yellow-700 hover:bg-yellow-500 text-black-50 py-3 rounded-lg transition"
           onClick={() => window.open(details.onlineApplicationUrl, '_blank')}
