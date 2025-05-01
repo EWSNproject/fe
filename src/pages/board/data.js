4a44bc094b8c8612b75470c80adc5e9e08403329
oh-chaeyeon <chaebread555@gmail.com>
2025-05-02 00:40:12 +0900

    chore : data.js 업데이트

--- src/pages/board/data.js
+++ src/pages/board/data.js
@@ -2,250 +2,432 @@ export const dummyBoardList = [
     {
       id: 31,
       title: '이제 성인이 되는 사람인데 추천할만 혜택있나요?',
+      content: `내년에 성인이 되는데, 청년 복지나 교육 지원 같은 혜택이 뭐가 있는지 잘 모르겠어요.
+혹시 추천해주실 만한 정책이나 사이트 있을까요?`,
       author: '채택이',
       date: '2025.02.17',
       views: 5,
       type: '질문',
+      good: 9,
+      link: {
+        label: '네이버',
+        url: 'https://www.naver.com/'
+      },
     },
     {
       id: 30,
       title: '대학생 무료 프로그램 뭐가 있을까요?',
+      content: `내년에 성인이 되는데, 청년 복지나 교육 지원 같은 혜택이 뭐가 있는지 잘 모르겠어요.
+혹시 추천해주실 만한 정책이나 사이트 있을까요?`,
       author: '혜택알리미',
       date: '2025.02.15',
       views: 12,
       type: '질문',
+      good: 0,
+      link: {
+        label: '네이버',
+        url: 'https://www.naver.com/'
+      },
     },
     {
       id: 29,
       title: '오늘 점심 추천해주세요!',
+      content: `내년에 성인이 되는데, 청년 복지나 교육 지원 같은 혜택이 뭐가 있는지 잘 모르겠어요.
+혹시 추천해주실 만한 정책이나 사이트 있을까요?`,
       author: '맛잘알',
       date: '2025.02.14', 
       views: 8,
       type: '자유',
+      good: 0,
+      link: {
+        label: '네이버',
+        url: 'https://www.naver.com/'
+      },
     },
     {
       id: 28,
       title: '처음 가입했어요! 반갑습니다!',
+      content: `내년에 성인이 되는데, 청년 복지나 교육 지원 같은 혜택이 뭐가 있는지 잘 모르겠어요.
+혹시 추천해주실 만한 정책이나 사이트 있을까요?`,
       author: '신입회원',
       date: '2025.02.13',
       views: 20,
       type: '인사',
+      good: 0,
+      link: {
+        label: '네이버',
+        url: 'https://www.naver.com/'
+      },
     },
     {
       id: 27,
       title: '정부 청년 지원금 총정리',
+      content: `내년에 성인이 되는데, 청년 복지나 교육 지원 같은 혜택이 뭐가 있는지 잘 모르겠어요.
+혹시 추천해주실 만한 정책이나 사이트 있을까요?`,
       author: '알뜰정보',
       date: '2025.02.12',
       views: 45,
       type: '질문',
+      good: 0,
+      link: {
+        label: '네이버',
+        url: 'https://www.naver.com/'
+      },
     },
     {
       id: 26,
       title: '토익 공부 어떻게 시작하시나요?',
+      content: `내년에 성인이 되는데, 청년 복지나 교육 지원 같은 혜택이 뭐가 있는지 잘 모르겠어요.
+혹시 추천해주실 만한 정책이나 사이트 있을까요?`,
       author: '스터디중',
       date: '2025.02.11',
       views: 18,
       type: '질문',
+      good: 0,
+      link: {
+        label: '네이버',
+        url: 'https://www.naver.com/'
+      },
     },
     {
       id: 25,
       title: '운동 루틴 공유합니다',
+      content: `내년에 성인이 되는데, 청년 복지나 교육 지원 같은 혜택이 뭐가 있는지 잘 모르겠어요.
+혹시 추천해주실 만한 정책이나 사이트 있을까요?`,
       author: '헬창',
       date: '2025.02.10',
       views: 30,
       type: '자유',
+      good: 0,
+      link: {
+        label: '네이버',
+        url: 'https://www.naver.com/'
+      },
     },
     {
       id: 24,
       title: '첫 글 남깁니다. 다들 안녕하세요!',
+      content: `내년에 성인이 되는데, 청년 복지나 교육 지원 같은 혜택이 뭐가 있는지 잘 모르겠어요.
+혹시 추천해주실 만한 정책이나 사이트 있을까요?`,
       author: '반가워요',
       date: '2025.02.09',
       views: 15,
       type: '인사',
+      good: 0,
+      link: {
+        label: '네이버',
+        url: 'https://www.naver.com/'
+      },
     },
     {
       id: 23,
       title: '2025년 복지 혜택 요약',
+      content: `내년에 성인이 되는데, 청년 복지나 교육 지원 같은 혜택이 뭐가 있는지 잘 모르겠어요.
+혹시 추천해주실 만한 정책이나 사이트 있을까요?`,
       author: '정보왕',
       date: '2025.02.08',
       views: 55,
       type: '질문',
+      good: 0,
+      link: {
+        label: '네이버',
+        url: 'https://www.naver.com/'
+      },
     },
     {
       id: 22,
       title: '다이어트 식단 추천 부탁드려요',
+      content: `내년에 성인이 되는데, 청년 복지나 교육 지원 같은 혜택이 뭐가 있는지 잘 모르겠어요.
+혹시 추천해주실 만한 정책이나 사이트 있을까요?`,
       author: '다이어터',
       date: '2025.02.07',
       views: 22,
       type: '자유',
+      good: 0,
+      link: {
+        label: '네이버',
+        url: 'https://www.naver.com/'
+      },
     },
     {
         id: 21,
         title: '이제 성인이 되는 사람인데 추천할만 혜택있나요?',
+        content: `내년에 성인이 되는데, 청년 복지나 교육 지원 같은 혜택이 뭐가 있는지 잘 모르겠어요.
+혹시 추천해주실 만한 정책이나 사이트 있을까요?`,
         author: '채택이',
         date: '2025.02.17',
         views: 5,
         type: '질문',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 20,
         title: '대학생 무료 프로그램 뭐가 있을까요?',
+        content: `내년에 성인이 되는데, 청년 복지나 교육 지원 같은 혜택이 뭐가 있는지 잘 모르겠어요.
+혹시 추천해주실 만한 정책이나 사이트 있을까요?`,
         author: '혜택알리미',
         date: '2025.02.15',
         views: 12,
         type: '질문',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 19,
         title: '오늘 점심 추천해주세요!',
+        content: `저는 다 좋아합니다.`,
         author: '맛잘알',
         date: '2025.02.14',
         views: 8,
         type: '자유',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 18,
         title: '처음 가입했어요! 반갑습니다!',
+        content: `안녕하세용!.`,
         author: '신입회원',
         date: '2025.02.13',
         views: 20,
         type: '인사',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 17,
         title: '정부 청년 지원금 총정리',
+        content: `안녕하세용!.`,
         author: '알뜰정보',
         date: '2025.02.12',
         views: 45,
         type: '질문',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 16,
         title: '토익 공부 어떻게 시작하시나요?',
+        content: `안녕하세용!.`,
         author: '스터디중',
         date: '2025.02.11',
         views: 18,
         type: '질문',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 15,
         title: '운동 루틴 공유합니다',
+        content: `안녕하세용!.`,
         author: '헬창',
         date: '2025.02.10',
         views: 30,
         type: '자유',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 14,
         title: '첫 글 남깁니다. 다들 안녕하세요!',
+        content: `안녕하세용!.`,
         author: '반가워요',
         date: '2025.02.09',
         views: 15,
         type: '인사',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 13,
         title: '2025년 복지 혜택 요약',
+        content: `안녕하세용!.`,
         author: '정보왕',
         date: '2025.02.08',
         views: 55,
         type: '질문',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 12,
         title: '다이어트 식단 추천 부탁드려요',
+        content: `안녕하세용!.`,
         author: '다이어터',
         date: '2025.02.07',
         views: 22,
         type: '자유',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 11,
         title: '다이어트 식단 추천 부탁드려요',
+        content: `안녕하세용!.`,
         author: '다이어터',
         date: '2025.02.07',
         views: 22,
         type: '자유',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 10,
         title: '다이어트 식단 추천 부탁드려요',
+        content: `안녕하세용!.`,
         author: '다이어터',
         date: '2025.02.07',
         views: 22,
         type: '자유',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 9,
         title: '다이어트 식단 추천 부탁드려요',
+        content: `안녕하세용!.`,
         author: '다이어터',
         date: '2025.02.07',
         views: 22,
         type: '자유',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 8,
         title: '다이어트 식단 추천 부탁드려요',
+        content: `안녕하세용!.`,
         author: '다이어터',
         date: '2025.02.07',
         views: 22,
         type: '자유',
+        good: 0,
       },
       {
         id: 7,
         title: '다이어트 식단 추천 부탁드려요',
+        content: `안녕하세용!.`,
         author: '다이어터',
         date: '2025.02.07',
         views: 22,
         type: '자유',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 6,
         title: '다이어트 식단 추천 부탁드려요',
+        content: `안녕하세용!.`,
         author: '다이어터',
         date: '2025.02.07',
         views: 22,
         type: '자유',
+        good: 0,
       },
       {
         id: 5,
         title: '다이어트 식단 추천 부탁드려요',
+        content: `안녕하세용!.`,
         author: '다이어터',
         date: '2025.02.07',
         views: 22,
         type: '자유',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 4,
         title: '다이어트 식단 추천 부탁드려요',
+        content: `안녕하세용!.`,
         author: '다이어터',
         date: '2025.02.07',
         views: 22,
         type: '자유',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 3,
         title: '다이어트 식단 추천 부탁드려요',
+        content: `안녕하세용!.`,
         author: '다이어터',
         date: '2025.02.07',
         views: 22,
         type: '자유',
+        good: 0,
+        link: {
+          label: '네이버',
+          url: 'https://www.naver.com/'
+        },
       },
       {
         id: 2,
         title: '다이어트 식단 추천 부탁드려요',
+        content: `안녕하세용!.`,
         author: '다이어터',
         date: '2025.02.07',
         views: 22,
         type: '자유',
+        good: 0,
       },
       {
         id: 1,
         title: '다이어트 식단 추천 부탁드려요',
+        content: `안녕하세용!.안녕하세용!.`,
         author: '다이어터',
         date: '2025.02.07',
         views: 22,
         type: '자유',
+        good: 0,
       },
 
   ];
